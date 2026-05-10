41e064f474a4629f80960f9493d871d06972bafb
MrtnDat25 <datgrb123456@gmail.com>
2026-05-10 15:48:13 +0700

    framing

---
+++ homomorphic_server.py
@@ -0,0 +1,18 @@
+import os 
+from config import *
+
+def aggregate_votes():
+
+    #read from public.text --> n ** n 
+    #check votes
+    #Calculate multiphication homomorphism
+    # E(m1+m2)  = E(m1).E(m2)
+
+    # save total to file homomorhic_vote
+    pass
+
+
+if __name__ == "__main__":
+    aggregate_votes()
+
+
